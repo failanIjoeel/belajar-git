9b3cb05300cb88690a0c8f27cbd6460f2d927ddb
failanIjoeel <69224688+failanIjoeel@users.noreply.github.com>
2020-08-07 22:38:11 +0700

    Add files via upload

---
+++ HelloKotlin.kt
@@ -0,0 +1,11 @@
+fun main () {
+    val name = "Failan Rahsya Izzul Pratama"
+    val hobi = "Menggambar dan Melukis"
+
+    print("Halo nama saya   :")
+    println(name)
+    print("Hobi saya adalah :")
+    println(hobi)
+    print(if (true)"always true" else "always false")
+
+}
